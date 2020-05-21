73a9b3df81423801442ca7dc4032bfcfc6c79133
Ivan0v42 <bazeeff@yahoo.com>
2020-05-21 11:57:47 +0300

    Initial Commit

---
+++ VkFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkFeed
+//
+//  Created by Ivan0v42 on 21.05.2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
